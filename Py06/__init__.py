c77bdd972db9785f9ac5da8fc7134bd34dadf9eb
Fernanda <febachman@gmail.com>
2026-09-05 21:11:15 -0300

    Py06 - Part IV

---
+++ Py06/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from .elements import create_air
+from .potions import healing_potion as heal, strength_potion
+from . import transmutation
+from . import grimoire
+
+__all__ = ["create_air", "heal", "strength_potion", "transmutation", "grimoire"]
