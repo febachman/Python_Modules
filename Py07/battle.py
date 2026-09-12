e3cac3fac173524b59778ba512dc934109ec6a9b
Fernanda <febachman@gmail.com>
2026-09-12 18:38:06 -0300

    Py07

--- Py07/battle.py
+++ Py07/battle.py
@@ -7,23 +7,25 @@ def test_factory(factory: CreatureFactory) -> None:
     print("Testing factory")
     base = factory.create_base()
     evolved = factory.create_evolved()
-    
+
     print(base.describe())
     print(base.attack())
     print(evolved.describe())
     print(evolved.attack())
 
+
 def test_battle(factory1: CreatureFactory, factory2: CreatureFactory) -> None:
     print("Testing battle")
     creature1 = factory1.create_base()
     creature2 = factory2.create_base()
-    
+
     print(creature1.describe())
     print(">>>>vs<<<<")
     print(creature2.describe())
     print("****fight****")
     print(creature1.attack())
     print(creature2.attack())
+
 
 if __name__ == "__main__":
     test_factory(FlameFactory())
--- Py07/tournament.py
+++ Py07/tournament.py
@@ -2,11 +2,15 @@
 
 from ex0 import CreatureFactory, FlameFactory, AquaFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
-from ex2 import BattleStrategy, NormalStrategy, AggressiveStrategy, DefensiveStrategy
+from ex2 import (
+    BattleStrategy, NormalStrategy, AggressiveStrategy, DefensiveStrategy
+)
 from typing import List, Tuple
 
 
-def tournament(opponents: List[Tuple[CreatureFactory, BattleStrategy]]) -> None:
+def tournament(
+    opponents: List[Tuple[CreatureFactory, BattleStrategy]]
+) -> None:
     print("*** Tournament ***")
     print(f"{len(opponents)} opponents involved")
 
@@ -15,16 +19,16 @@ def tournament(opponents: List[Tuple[CreatureFactory, BattleStrategy]]) -> None:
             print("\n* Battle *")
             factory1, strategy1 = opponents[i]
             factory2, strategy2 = opponents[j]
-            
+
             c1 = factory1.create_base()
             c2 = factory2.create_base()
-            
+
             try:
                 print(c1.describe())
                 print("vs.")
                 print(c2.describe())
                 print("now fight!")
-                
+
                 print(strategy1.act(c1))
                 print(strategy2.act(c2))
             except ValueError as e:
