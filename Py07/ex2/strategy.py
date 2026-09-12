129300a1c9803326b932d88aae2ac8d7b9594af8
Fernanda <febachman@gmail.com>
2026-09-12 18:25:44 -0300

    Py07

---
+++ Py07/ex2/strategy.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+from ex0.creatures import Creature
+from ex1.healing import HealCapability
+from ex1.transforming import TransformCapability
+
+class BattleStrategy(ABC):
+
+    @abstractmethod
+    def is_valid(self, creature: Creature) -> bool:
+        pass
+
+    @abstractmethod
+    def act(self, creature: Creature) -> str:
+        pass
+
+
+class NormalStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return True
+    
+    def act(self, creature: Creature) -> str:
+        return creature.attack()
+
+
+class AggressiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        if isinstance(creature, TransformCapability):
+            return True
+        return False
+
+    def act(self, creature: Creature) -> str:
+        if not self.is_valid(creature):
+            raise ValueError(f"Invalid Creature '{creature.name}' for this aggressive strategy")
+        return f"{creature.transform()}\n{creature.attack()}\n{creature.revert()}"
+
+
+class DefensiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        if isinstance(creature, HealCapability):
+            return True
+        return False
+
+    def act(self, creature: Creature) -> str:
+        if not self.is_valid(creature):
+            raise ValueError(f"Invalid Creature '{creature.name}' for this defensive strategy")
+        return f"{creature.attack()}\n{creature.heal()}"
---
+++ Py07/tournament.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from ex0 import CreatureFactory, FlameFactory, AquaFactory
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+from ex2 import BattleStrategy, NormalStrategy, AggressiveStrategy, DefensiveStrategy
+from typing import List, Tuple
+
+
+def tournament(opponents: List[Tuple[CreatureFactory, BattleStrategy]]) -> None:
+    print("*** Tournament ***")
+    print(f"{len(opponents)} opponents involved")
+
+    for i in range(len(opponents)):
+        for j in range(i + 1, len(opponents)):
+            print("\n* Battle *")
+            factory1, strategy1 = opponents[i]
+            factory2, strategy2 = opponents[j]
+            
+            c1 = factory1.create_base()
+            c2 = factory2.create_base()
+            
+            try:
+                print(c1.describe())
+                print("vs.")
+                print(c2.describe())
+                print("now fight!")
+                
+                print(strategy1.act(c1))
+                print(strategy2.act(c2))
+            except ValueError as e:
+                print(f"Battle error, aborting tournament: {e}")
+                return
+
+
+if __name__ == "__main__":
+    print("Tournament 0 (basic)")
+    print("[(Flameling+Normal), (Healing+Defensive)]")
+    opponents_0 = [
+        (FlameFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy())
+    ]
+    tournament(opponents_0)
+
+    print("\nTournament 1 (error)")
+    print("[ (Flameling+Aggressive), (Healing+Defensive)]")
+    opponents_1 = [
+        (FlameFactory(), AggressiveStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy())
+    ]
+    tournament(opponents_1)
+
+    print("\nTournament 2 (multiple)")
+    print("[ (Aquabub+Normal), (Healing+Defensive), (Transform+Aggressive)]")
+    opponents_2 = [
+        (AquaFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy()),
+        (TransformCreatureFactory(), AggressiveStrategy())
+    ]
+    tournament(opponents_2)
